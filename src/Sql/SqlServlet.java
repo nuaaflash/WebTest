032003fb059b70da6835aeda8e1995af545066b7
161530319-XiaHan <xh.yes@qq.com>
2017-12-11 12:59:14 +0800

    Remove a "//TODO"

--- src/Sql/SqlServlet.java
+++ src/Sql/SqlServlet.java
@@ -76,7 +76,6 @@ public class SqlServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
