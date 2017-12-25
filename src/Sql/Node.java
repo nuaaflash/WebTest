6d16c0ed0832f096d926744b8aff9ed8562f20c6
161530319-XiaHan <xh.yes@qq.com>
2017-12-25 22:18:32 +0800

    1.给数据库和Node添加了节点层次信息 2.添加了返回叶子节点名字的集合的方法 参数是树的名字 3.添加了返回非叶子节点名字集合的方法 参数也是树名字 4.添加了返回最大的子节点的数量的方法 参数还是树名字 5.根据1重写了sql脚本 6.根据1重写了新版树更新的前端JS

--- src/Sql/Node.java
+++ src/Sql/Node.java
@@ -6,14 +6,16 @@ public class Node{
 	public int numofChildren;
 	public int parentId;
 	public double value;
-	public Node(String nodeName,int parentId,double value,int nodeId, int numofChildren){
+	public int level;
+	public Node(String nodeName,int parentId,double value,int nodeId, int numofChildren,int level){
 		this.nodeName = nodeName;
 		this.parentId = parentId;
 		this.value = value;
 		this.nodeId = nodeId;
 		this.numofChildren = numofChildren;
+		this.level = level;
 	}
 	public void ShowNode(){
-		System.out.println("node_name:"+nodeName+" parent_id:"+parentId+" node_value:"+value+" node_id:"+nodeId+" num_of_children:"+numofChildren);
+		System.out.println("node_name:"+nodeName+" parent_id:"+parentId+" node_value:"+value+" node_id:"+nodeId+" num_of_children:"+numofChildren+"node_level"+level);
 	}
 }
--- src/Sql/SqlServlet.java
+++ src/Sql/SqlServlet.java
@@ -92,7 +92,7 @@ public class SqlServlet extends HttpServlet {
 				sql.setInstruct(instructions);
 				sql.doInstructions();
 				
-				// 调整子节点被操作的父节点的 num_of_children值
+				// 调整子节点被操作的父节点的 num_of_children值 以及被添加节点的层次信息
 				
 				for(int i=0;i<Integer.parseInt(request.getParameter("length1"));i++) {
 					int addc = Integer.parseInt(request.getParameter("childadded"+i));
